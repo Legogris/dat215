6f4b4a9abf0ce03ca40054253843d775b01d0ac5
Legogris <legogris@legogris.se>
2013-02-15 16:11:28 +0100

    Added Logic.CreditCard class

---
+++ iMat/Logic/CreditCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public enum CardType
+    {
+        Visa,
+        MasterCard,
+        Amex
+    }
+    class CreditCard
+    {
+        public CardType CardType { get; set; }
+        public string CardNumber { get; set; }
+        public string HoldersName { get; set; }
+        public int ValidMonth { get; set; }
+        public int ValidYear { get; set; }
+        public int VerificationCode { get; set; }
+    }
+}
