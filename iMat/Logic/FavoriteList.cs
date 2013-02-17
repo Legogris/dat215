6a89c0a756e92227e652f9cf24d71b5fefce97a3
Legogris <legogris@legogris.se>
2013-02-17 12:23:04 +0100

    Renaming Logic to Data

---
+++ iMat/Logic/FavoriteList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class FavoriteList 
+    {
+        private readonly List<ShoppingItem> items;
+        public string Name { get; private set; }
+        public FavoriteList(string name)
+        {
+            items = new List<ShoppingItem>();
+            Name = name;
+        }
+        public void Add(ShoppingItem item)
+        {
+            items.Add(item);
+        }
+        public void Remove(ShoppingItem item)
+        {
+            items.Remove(item);
+        }
+        public IEnumerator<ShoppingItem> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
--- iMat/Logic/ShoppingItem.cs
+++ iMat/Logic/ShoppingItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logic
+namespace Data
 {
     public class ShoppingItem
     {
@@ -16,6 +16,48 @@ namespace Logic
         {
             Product = product;
             Amount = amount;
+        }
+
+        public override int GetHashCode()
+        {
+            return Product.GetHashCode() * 3 + Amount.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            ShoppingItem si = obj as ShoppingItem;
+            return Equals(si);
+        }
+
+        public bool Equals(ShoppingItem si)
+        {
+            if ((object)si == null)
+            {
+                return false;
+            }
+            return si.Amount == Amount && si.Product == Product;
+        }
+
+        public static bool operator ==(ShoppingItem a, ShoppingItem b)
+        {
+            if (System.Object.ReferenceEquals(a, b))
+            {
+                return true;
+            }
+            if ((object)a == null || (object)b == null)
+            {
+                return false;
+            }
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ShoppingItem a, ShoppingItem b)
+        {
+            return !(a == b);
         }
     }
 }
