eda719dd526f15f94321b20113b4c900e0aac58c
Legogris <legogris@legogris.se>
2013-02-24 15:26:19 +0100

    Class dependent TreeViewItem

---
+++ iMat/UI.Desktop/Content/Controls/TreeViewCategoryItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.Desktop.Content.Controls
+{
+    /// <summary>
+    /// Interaction logic for TreeViewCategoryItem.xaml
+    /// </summary>
+    public partial class TreeViewCategoryItem : AbstractTreeViewItem
+    {
+        public TreeViewCategoryItem()
+        {
+            InitializeComponent();
+            this.DataContextChanged += TreeViewCategoryItem_DataContextChanged;
+        }
+
+        void TreeViewCategoryItem_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ProductCategory pc = e.NewValue as ProductCategory;
+            if (pc != null)
+            {
+                nameLabel.Content = pc.Name;
+            }
+        }
+    }
+}
---
+++ iMat/UI.Desktop/Content/Controls/TreeViewProductItem.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.Desktop.Content.Controls
+{
+    /// <summary>
+    /// Interaction logic for TreeViewProductItem.xaml
+    /// </summary>
+    public partial class TreeViewProductItem : AbstractTreeViewItem
+    {
+        public TreeViewProductItem()
+        {
+            InitializeComponent();
+            DataContextChanged += TreeViewProductItem_DataContextChanged;
+        }
+
+        void TreeViewProductItem_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Product p = e.NewValue as Product;
+            if (p != null)
+            {
+                nameLabel.Content = p.Name;
+                priceLabel.Content = p.Price;
+            }
+        }
+    }
+}
