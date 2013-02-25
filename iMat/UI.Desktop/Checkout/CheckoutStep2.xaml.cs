8326aa2ff163b7d77a97c3f41f2041ffbc57e889
Viktor Karlsson <wiixtor@gmail.com>
2013-02-25 17:04:48 +0100

    Started on CheckoutWizard

---
+++ iMat/UI.Desktop/Checkout/CheckoutStep2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.Desktop.Checkout
+{
+    /// <summary>
+    /// Interaction logic for CheckoutStep2.xaml
+    /// </summary>
+    public partial class CheckoutStep2 : UserControl
+    {
+        public CheckoutStep2()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeDelivery_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PickupInStore_Checked(object sender, RoutedEventArgs e)
+        {
+            StoreComboBox.IsEnabled = true;
+        }
+    }
+}
